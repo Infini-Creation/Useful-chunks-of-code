85f1f446349e62f3583f076b38ccef19d4a5e2a0
Infini-Creation <infini.creation.france@gmail.com>
2025-05-28 10:09:42 +0200

    add a "menu" type scene

---
+++ advanced-drag-and-drop/scene_selection.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+var one_dropspot_scene = preload("res://one_dropspot.tscn")
+var rotated_dropspot_scene = preload("res://rotatedCD_scene.tscn")
+var multiple_dropspots_scene = preload("res://many_cards_and_drop_spots.tscn")
+
+var od_inst
+var rd_inst
+var md_inst
+
+
+func _process(_delta: float) -> void:
+	if od_inst != null:
+		if od_inst.get_back == true:
+			if od_inst.is_inside_tree() == true:
+				od_inst.get_back = false
+				remove_child(od_inst)
+			$CenterContainer/HBoxContainer.show()
+
+	if rd_inst != null:
+		if rd_inst.get_back == true:
+			if rd_inst.is_inside_tree() == true:
+				rd_inst.get_back = false
+				remove_child(rd_inst)
+			$CenterContainer/HBoxContainer.show()
+
+	if md_inst != null:
+		if md_inst.get_back == true:
+			if md_inst.is_inside_tree() == true:
+				md_inst.get_back = false
+				remove_child(md_inst)
+			$CenterContainer/HBoxContainer.show()
+
+
+func _on_button_pressed() -> void:
+	od_inst = one_dropspot_scene.instantiate()
+	$CenterContainer/HBoxContainer.hide()
+	add_child(od_inst)
+
+
+func _on_button_2_pressed() -> void:
+	rd_inst = rotated_dropspot_scene.instantiate()
+	$CenterContainer/HBoxContainer.hide()
+	add_child(rd_inst)
+
+
+func _on_button_3_pressed() -> void:
+	md_inst = multiple_dropspots_scene.instantiate()
+	$CenterContainer/HBoxContainer.hide()
+	add_child(md_inst)

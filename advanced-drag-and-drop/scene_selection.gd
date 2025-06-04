806cbb828a12a04f586e5b583201640f30a19793
Infini-Creation <infini.creation.france@gmail.com>
2025-06-04 23:45:13 +0200

    add new scene in menu

--- advanced-drag-and-drop/scene_selection.gd
+++ advanced-drag-and-drop/scene_selection.gd
@@ -3,11 +3,12 @@ extends Node2D
 var one_dropspot_scene = preload("res://one_dropspot.tscn")
 var rotated_dropspot_scene = preload("res://rotatedCD_scene.tscn")
 var multiple_dropspots_scene = preload("res://many_cards_and_drop_spots.tscn")
+var mdp_with_card_type_scene = preload("res://many_cards_and_drop_spots_ct.tscn")
 
 var od_inst
 var rd_inst
 var md_inst
-
+var mdct_inst
 
 func _process(_delta: float) -> void:
 	if od_inst != null:
@@ -31,6 +32,13 @@ func _process(_delta: float) -> void:
 				remove_child(md_inst)
 			$CenterContainer/HBoxContainer.show()
 
+	if mdct_inst != null:
+		if mdct_inst.get_back == true:
+			if mdct_inst.is_inside_tree() == true:
+				mdct_inst.get_back = false
+				remove_child(mdct_inst)
+			$CenterContainer/HBoxContainer.show()
+
 
 func _on_button_pressed() -> void:
 	od_inst = one_dropspot_scene.instantiate()
@@ -48,3 +56,9 @@ func _on_button_3_pressed() -> void:
 	md_inst = multiple_dropspots_scene.instantiate()
 	$CenterContainer/HBoxContainer.hide()
 	add_child(md_inst)
+
+
+func _on_button_4_pressed() -> void:
+	mdct_inst = mdp_with_card_type_scene.instantiate()
+	$CenterContainer/HBoxContainer.hide()
+	add_child(mdct_inst)
